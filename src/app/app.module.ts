9e281c0f6dc9ec781b290c5c7764e932a6a4fc11
Gerson Sánchez <gb.so.dev@outlook.com>
2023-07-07 21:43:53 -0500

    modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibraryModule } from './library/library.module';
 import { HttpClientModule } from '@angular/common/http';
 import { loadConfigProvider } from './app-initializer/app.initializer';
 
@@ -13,7 +14,8 @@ import { loadConfigProvider } from './app-initializer/app.initializer';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    LibraryModule
   ],
   providers: [loadConfigProvider
   ],
