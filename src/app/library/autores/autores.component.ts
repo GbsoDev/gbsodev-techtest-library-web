7de27ad0498695cd0d89bb3f2d09c14038905a66
Gerson Sánchez <gb.so.dev@outlook.com>
2023-07-13 22:45:53 -0500

    actualizando componetes de autores

--- src/app/library/autores/autores.component.ts
+++ src/app/library/autores/autores.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Autor } from 'src/app/model/autor';
 import { AutorService } from 'src/app/services/autor.service';
 
@@ -8,9 +8,9 @@ import { AutorService } from 'src/app/services/autor.service';
   styleUrls: ['./autores.component.scss']
 })
 export class AutoresComponent implements OnInit {
-  updating:boolean=false;
-  watching:boolean=false;
+  @ViewChild('formModal') modalRef: any;
   autores: Autor[] = [];
+  formMode?: string;
 
   constructor(private autorService: AutorService) { }
 
@@ -19,29 +19,31 @@ export class AutoresComponent implements OnInit {
   }
 
   loadAutores() {
-    this.autorService.list().subscribe(
-      {
-        next: (value: Autor[]) => {
-          this.autores = value;
-        },
-        error: (error: any) => {
-          console.log(error);
-        },
-        complete: () => {
-
-        }
-      }
-    );
-  }
-  deleteAutor(autor: Autor) {
-    this.autorService.delete(autor.id!).subscribe({
-      next: (response: any) => {
-        console.log('Autor eliminado');
-        this.loadAutores();
+    this.autorService.list().subscribe({
+      next: (value: Autor[]) => {
+        this.autores = value;
       },
       error: (error: any) => {
-        console.log('Error al eliminar autor', error);
+        console.log('Error al consultar autores', error);
       }
     });
+  }
+
+  deleteAutor(autor: Autor) {
+    if (autor.id != null) {
+      this.autorService.delete(autor.id).subscribe({
+        next: (response: any) => {
+          console.log('Autor eliminado');
+          this.loadAutores();
+        },
+        error: (error: any) => {
+          console.log('Error al eliminar autor', error);
+        }
+      });
+    }
+  }
+
+  closeModal() {
+    this.loadAutores();
   }
 }
